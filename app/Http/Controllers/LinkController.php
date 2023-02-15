74b64fbb61034e4d8d2c7b946f5b6734066d9cf9
Adamity <50904284+adamity@users.noreply.github.com>
2023-02-15 22:50:24 +0800

    update encoder function by using uuid as hash instead of convert id to base 62, avoid duplicate origin url, add renew expired date function

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Link;
 use Carbon\Carbon;
+use Ramsey\Uuid\Uuid;
 
 class LinkController extends Controller
 {
@@ -12,62 +13,67 @@ class LinkController extends Controller
     // - Task Scheduling : Delete unused link after 30 days starting form updated date
     public function encoder(Request $request)
     {
-        // Request original URL from form
         $this->validate($request, ['origin_link' => 'required']);
         $origin_link = $request->input('origin_link');
 
         // Remove all illegal characters from a URL
         $origin_link = filter_var($origin_link, FILTER_SANITIZE_URL);
+
         // Validate URL
-        if(filter_var($origin_link,FILTER_VALIDATE_URL)!==false)
-        {
-            // Store valid URL into database & get the ID
-            $link = new Link;
-            $link->origin_link = $origin_link;
-            $link->save();
-            $id = $link->id;
+        if (filter_var($origin_link, FILTER_VALIDATE_URL)) {
+            $link = Link::where('origin_link', '=', $origin_link)->first();
 
-            // Convert the ID(base 10) into hash(base 62) form & store into database
-            $map = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            $hash = '';
-            do {
-                $hash = $map[$id % 62].$hash;
-                $id = intval($id / 62);
-            } while($id);
-            $link->hash = $hash;
+            if (!$link) {
+                $link = new Link;
+                $link->origin_link = $origin_link;
+                $link->hash = $this->generate_uuid();
+            }
+
+            $link->expired_at = $this->renew_expired();
             $link->save();
 
-            // Return URL_base(tinyus.test/)+base62 into web page
-            $shorten_link = 'tinyus.test/'.$hash;
-            return view('page.index')->with('shorten_link',$shorten_link);
-        }
-        else
-        {
-            return redirect('/')->with('error','Unable to shorten that link. It is not a valid url.');
+            $shorten_link = "tinyus.test/$link->hash";
+            return view('page.index')->with('shorten_link', $shorten_link);
+        } else {
+            return redirect('/')->with('error', 'Unable to shorten that link. It is not a valid url.');
         }
     }
 
     public function decoder($hash)
     {
-        // Search the hash(base 62) in the database that match with base 62 from tinyurl
         $link = Link::where('hash', '=', $hash)->first();
-        if($link != null)
-        {
+        if ($link) {
             // Update updated_at & expired_at attribute
             $updated = Carbon::now()->toDateTimeString();
-            $expired = date('Y-m-d', strtotime($updated. ' +30 days'));
             $link->updated_at = $updated;
-            $link->expired_at = $expired;
+            $link->expired_at = $this->renew_expired();
             $link->save();
 
             // Get the original URL & redirect to the original URL page
             $origin_link = $link->origin_link;
             return redirect()->to($origin_link);
-        }
-        else
-        {
+        } else {
             // If hash not found, throw error message
-            return redirect('/')->with('error','This is a 404 error, which means you have clicked on a bad link or entered an invalid URL.');
+            return redirect('/')->with('error', 'This is a 404 error, which means you have clicked on a bad link or entered an invalid URL.');
         }
+    }
+
+    private function generate_uuid()
+    {
+        $uuid = Uuid::uuid4();
+        $short_uuid = substr($uuid->toString(), 0, 8);
+
+        $is_exist = Link::where('hash', '=', $short_uuid)->first();
+        if (!$is_exist) return $short_uuid;
+
+        return $this->generate_uuid();
+    }
+
+    private function renew_expired()
+    {
+        $date = Carbon::now()->toDateTimeString();
+        $expired = date('Y-m-d', strtotime($date . ' +30 days'));
+
+        return $expired;
     }
 }
