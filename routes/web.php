a71f28cb2666338d2a465443d7774ac3aa751f2a
Adamity <50904284+adamity@users.noreply.github.com>
2023-02-15 20:33:43 +0800

    update ...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
-Route::get('/',function(){return view('page.index');});
-Route::get('/{link}','LinkController@decoder');
-Route::post('/','LinkController@encoder')->name('encoder');
+Route::get('/', function () {
+    return view('page.index');
+});
+
+Route::get('/{link}', 'LinkController@decoder');
+Route::post('/', 'LinkController@encoder')->name('encoder');
